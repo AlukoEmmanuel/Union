76ff26018c89c97893fb214884d110a7b973deeb
AlukoEmmanuel <alukoooreoluwa31@gmail.com>
2026-08-15 18:37:36 +0100

    Union of sets

---
+++ union.py
@@ -0,0 +1,9 @@
+set1 = {'A', 'B', 'C', 'D', 'E'}
+set2 = {'B', 'D', 'V', 'X', 'Y', 'Z'}
+
+# FInd union  for two sets
+union = set1.union(set2)
+# list
+total_guests = list(union)
+print('Total guests to be invited in the party are:', len(total_guests))
+print('Guest List:', total_guests)
